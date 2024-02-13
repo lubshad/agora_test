704acad0175156148bd3abdca6650875a73df48e
lubshad <lubshad4u4@gmail.com>
2024-02-13 22:29:22 +0530

    Import Fix

--- lib/features/authentication/phone_or_google_auth/phone_or_google_auth_screen.dart
+++ lib/features/authentication/phone_or_google_auth/phone_or_google_auth_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pinput/pinput.dart';
-import 'package:social_auth_buttons/res/buttons/google_auth_button.dart';
-import 'package:social_auth_buttons/res/shared/auth_button_style.dart';
 import 'package:video_call_agora/features/authentication/phone_or_google_auth/google_oauth_mixin.dart';
 import 'package:video_call_agora/widgets/app_logo.dart';
 import '../../../exporter.dart';
